e6c10cd9774271f91885ca5f83bfb97e4b14f261
yibaebi <elliot.yibaebi@yahoo.com>
2022-11-11 14:46:47 +0100

    add website assets

---
+++ src/assets/images/index.js
@@ -0,0 +1,40 @@
+import aboutEventImgBg from './about_event__bg.png'
+import aboutEventImg from './about_event__image.png'
+import chiomaJesus from './Chioma Jesus.png'
+import donJazzy from './Don Jazzy.png'
+import foundationImg from './foundation_pic.png'
+import heroImage from './hero_image.png'
+import kaffy from './Kaffy.png'
+import kunleAfolayan from './Kunle Afolayan.png'
+import learnFromImg from './learn_from__image.png'
+import mi from './MI.png'
+import moAbudu from './Mo Abudu.png'
+import phyno from './Phyno.png'
+import pSquare from './PSquare.png'
+import timiDakolo from './Timi Dakolo.png'
+import tuFace from './Tuface.png'
+import tyBello from './Ty Bello.png'
+
+import watchFavsImg from './watch_favs__image.png'
+import winPrizesImg from './win_image.png'
+
+export {
+  tuFace,
+  timiDakolo,
+  pSquare,
+  chiomaJesus,
+  phyno,
+  moAbudu,
+  tyBello,
+  mi,
+  kunleAfolayan,
+  foundationImg,
+  watchFavsImg,
+  winPrizesImg,
+  donJazzy,
+  kaffy,
+  learnFromImg,
+  aboutEventImg,
+  aboutEventImgBg,
+  heroImage,
+}
---
+++ src/assets/svg/index.js
@@ -0,0 +1,8 @@
+import { ReactComponent as ButtonHighlight } from './button_highlight.svg'
+import { ReactComponent as LogoIpsum } from './logo_ipsum.svg'
+import { ReactComponent as LogoIpsum2 } from './logo_ipsum__2.svg'
+import { ReactComponent as LogoIpsum3 } from './logo_ipsum__3.svg'
+import { ReactComponent as PrimaryLogo } from './primary_logo.svg'
+import { ReactComponent as RightArrow } from './right_arrow.svg'
+
+export { LogoIpsum3, LogoIpsum2, LogoIpsum, ButtonHighlight, RightArrow, PrimaryLogo }
