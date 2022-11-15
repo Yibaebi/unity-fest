5e3bc103f70fc53533eaea4f8e72bccdc6d93b5d
yibaebi <elliot.yibaebi@yahoo.com>
2022-11-15 12:06:20 +0100

    optimized assets

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,22 +1,21 @@
-import aboutEventImgBg from './about_event__bg.png'
-import aboutEventImg from './about_event__image.png'
-import chiomaJesus from './Chioma Jesus.png'
-import donJazzy from './Don Jazzy.png'
-import foundationImg from './foundation_pic.png'
-import heroImage from './hero_image.png'
-import kaffy from './Kaffy.png'
-import kunleAfolayan from './Kunle Afolayan.png'
-import learnFromImg from './learn_from__image.png'
-import mi from './MI.png'
-import moAbudu from './Mo Abudu.png'
-import phyno from './Phyno.png'
-import pSquare from './PSquare.png'
-import timiDakolo from './Timi Dakolo.png'
-import tuFace from './Tuface.png'
-import tyBello from './Ty Bello.png'
+import aboutEventImg from './about_event__image.webp'
+import chiomaJesus from './Chioma Jesus.webp'
+import donJazzy from './Don Jazzy.webp'
+import foundationImg from './foundation_pic.webp'
+import heroImage from './hero_image.webp'
+import kaffy from './Kaffy.webp'
+import kunleAfolayan from './Kunle Afolayan.webp'
+import learnFromImg from './learn_from__image.webp'
+import mi from './MI.webp'
+import moAbudu from './Mo Abudu.webp'
+import phyno from './Phyno.webp'
+import pSquare from './PSquare.webp'
+import timiDakolo from './Timi Dakolo.webp'
+import tuFace from './Tuface.webp'
+import tyBello from './Ty Bello.webp'
 
-import watchFavsImg from './watch_favs__image.png'
-import winPrizesImg from './win_image.png'
+import watchFavsImg from './watch_favs__image.webp'
+import winPrizesImg from './win_image.webp'
 
 export {
   tuFace,
@@ -35,6 +34,5 @@ export {
   kaffy,
   learnFromImg,
   aboutEventImg,
-  aboutEventImgBg,
   heroImage,
 }
